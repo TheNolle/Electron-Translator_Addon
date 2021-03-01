2898b916bbb55e5d0f17b63e292475f825c53057
TheNolle <61505071+TheNolle@users.noreply.github.com>
2021-03-01 19:36:16 +0100

    Add files via upload

---
+++ translator/js/home.js
@@ -0,0 +1,66 @@
+var langMap = new Map(); // New map for all langs
+const fs = require('fs');
+
+function initLang() {
+
+    const langFiles = fs.readdirSync('./translator/langs/').filter(file => file.endsWith('.json')); //Get all langs files .json
+
+    var langSelect = document.getElementById("lang-select");
+    var langNumber = 0;
+    var length = langSelect.options.length;
+    for (i = length - 1; i >= 0; i--) {
+        langSelect.options[i] = null; //Clear all option
+    }
+    for (const file of langFiles) { //Execute for each lang file
+        langNumber++;
+        const lang = require('./translator/langs/' + file);
+        var option = document.createElement('option'); //Create the option
+        option.text = lang.name; //Set option
+        langSelect.add(option); //Add the option
+        langMap.set(lang.name, file);
+    }
+    if (localStorage.getItem('lang') && localStorage.getItem('lang-file')) { // if have localstorage
+
+        document.getElementById('lang-select').value = localStorage.getItem('lang'); // Set selector option to the lang selected
+        const fs = require('fs');
+        fs.readFile('./translator/langs/' + localStorage.getItem("lang-file"), 'utf8', (err, data) => { // Read the lang file selectioned
+            if (err) {
+                console.error(err) // Log if have a error
+                return
+            }
+            const file = JSON.parse(data); // Parse the json
+            traduction("LangsTitlePopup", file.LangsTitlePopup);
+            traduction("ExampleTitle", file.ExampleTitle);
+            traduction("ExampleText", file.ExampleText);
+
+            traduction("IncorporationFooter", file.IncorporationFooter);
+            traduction("AboutFooter", file.AboutFooter);
+            traduction("DataUsageFooter", file.DataUsageFooter);
+            traduction("HowToUseFooter", file.HowToUseFooter);
+            traduction("ChangeLanguageFooter", file.ChangeLanguageFooter);
+
+            traduction("SocialLinks", file.SocialLinks);
+            traduction("NewsBody", file.NewsBody);
+
+            traduction("NewsTitle1", file.NewsTitle1);
+            traduction("1NewsText1", file.NewsText11);
+            traduction("2NewsText1", file.NewsText12);
+            traduction("ReadTheArticle", file.ReadTheArticle);
+        })
+    }
+}
+
+function validateLang() { // When button done is pressed
+    const langSelect = document.getElementById("lang-select"); // Get selector
+    localStorage.removeItem('lang'); // Clear localstorage
+    localStorage.removeItem('lang-file'); // Clear localstorage
+    localStorage.setItem('lang', langSelect.value); // Add the lang name at localstorage
+    localStorage.setItem('lang-file', langMap.get(langSelect.value)); // Add the lang name at localstorage
+    console.log(localStorage.getItem('lang')); // Log to console
+    console.log(localStorage.getItem('lang-file')); // Log to console
+    initLang() //Re init for save change
+}
+
+function traduction(id, key) {
+    document.getElementById(id).innerHTML = key;
+}
