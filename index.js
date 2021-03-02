6bf6c137b2d93acac7736da1a6103ad5d04854fb
TheNolle <61505071+TheNolle@users.noreply.github.com>
2021-03-02 10:21:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 // Importation des modules
 const path = require("path");
-const { Client, Authenticator } = require("minecraft-launcher-core");
 const { app, ipcMain, shell, BrowserWindow } = require("electron");
-const fs = require("fs");
 
 
 
 
 // Variables globales
 var mainWindow;
-var userAuth = null;
-const launcher = new Client();
 
 
 
@@ -58,57 +54,4 @@ app.whenReady().then(() => {
 // Si toutes les fenêtres sont fermées, quitter l'application
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
-});
-
-
-
-
-// Vérification du Mail et du Mot de Passe Mojang de l'utilisateur pour la connexion à "TheNolle's Realms"
-ipcMain.on("mojang-login", (evt, data) => {
-  Authenticator.getAuth(data.mail, data.pass)
-    .then((auth) => {
-      userAuth = auth;
-      mainWindow.loadURL(path.join(__dirname, "pages/global/choose-app.html")).then(()=>{
-        mainWindow.webContents.send('uuid', auth.uuid);
-      });
-    })
-    .catch((err) => {
-      mainWindow.webContents.send('loginError');
-    })
-});
-// Déconnexion du joueur en cliquant "Disconnect"
-ipcMain.on("logout", () => {
-  if (userAuth != null) {
-    Authenticator.invalidate(userAuth.access_token, userAuth.client_token);
-    userAuth = null;
-  }
-});
-
-
-
-
-// Lancer "TheNolle's Realms"
-ipcMain.on("launch-realms", () => {
-  if (userAuth != null) {
-    let DataFolder = (process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")) + "/TnStudios";
-    console.log("APPDATA =" + DataFolder)
-    if (!fs.existsSync(DataFolder)) {
-      fs.mkdirSync(DataFolder);
-    };
-    let opts = {
-      clientPackage: null,
-      authorization: userAuth,
-      root: DataFolder + "/Realms",
-      version: {
-        number: "1.16.5",
-        type: "release"
-      },
-      memory: {
-        max: "4G",
-        min: "2G"
-      }
-    }
-
-    launcher.launch(opts);
-  }
 });
